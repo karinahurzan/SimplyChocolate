7275975e2767785630b48dc66c304400f1ccae9c
hurzan.karina <kgurzan@gmail.com>
2025-02-11 10:49:59 +0000

    add chocolate goit project

---
+++ js/modal.js
@@ -0,0 +1,24 @@
+(() => {
+  const openModalBtns = document.querySelectorAll("[data-modal-open]");
+  const closeModalBtns = document.querySelectorAll("[data-modal-close]");
+
+  openModalBtns.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      const modalId = btn.getAttribute("data-modal-open");
+      const modal = document.querySelector(`[data-modal="${modalId}"]`);
+      toggleModal(modal);
+    });
+  });
+
+  closeModalBtns.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      const modalId = btn.getAttribute("data-modal-close");
+      const modal = document.querySelector(`[data-modal="${modalId}"]`);
+      toggleModal(modal);
+    });
+  });
+
+  function toggleModal(modal) {
+    modal.classList.toggle("is-open");
+  }
+})();
